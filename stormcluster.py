Fix quantile normalisation logic for blank images

stormcluster.py
@@ -91,11 +91,10 @@ def _stretchlim(image, bottom=0.001, top=None, in_place=True):
         out[:] = image
     if top is None:
         top = 1 - bottom
-    if not np.any(out):
-        return out  # protect against empty images
     q0, q1 = np.percentile(image, [100*bottom, 100*top])
     out -= q0
-    out /= q1 - q0
+    if q1 > q0:
+        out /= q1 - q0
     out = np.clip(out, 0, 1, out=out)
     return out
 